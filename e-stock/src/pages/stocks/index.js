ed26ad5d9c5492251d17e1a7df87aae803de6e4b
Mark Dave <dave.lorejo69@gmail.com>
2023-03-28 23:36:18 +0800

    made quantity editable

--- e-stock/src/pages/stocks/index.js
+++ e-stock/src/pages/stocks/index.js
@@ -1,12 +1,10 @@
-import './styles.css'
-import "@fontsource/inter";
-import Sidebar from '../../sidebar'
-import { FaSearch } from 'react-icons/fa';
 import React, { useState } from 'react';
+import './styles.css';
+import Sidebar from '../../sidebar';
+import { FaSearch } from 'react-icons/fa';
 import AddProductModal from './AddProductModal';
 import ProductTable from './table';
 import { isWithinInterval, addWeeks } from 'date-fns';
-
 
 
 export default function Stocks() {
@@ -25,19 +23,43 @@ export default function Stocks() {
 
   const addProduct = (product) => {
     setProducts([...products, product]);
-  
+
     if (filteredProducts === products) {
       setFilteredProducts([...products, product]);
     } else if (filteredProducts[0].category === product.category) {
       setFilteredProducts([...filteredProducts, product]);
     }
   };
-   
 
-  
+  const updateProductQuantity = (productId, newQuantity) => {
+    const updatedProducts = products.map((product) => {
+      if (product.id === productId) {
+        return { ...product, quantity: newQuantity };
+      } else {
+        return product;
+      }
+    });
+
+    setProducts(updatedProducts);
+
+    if (filteredProducts === products) {
+      setFilteredProducts(updatedProducts);
+    } else {
+      const updatedFilteredProducts = filteredProducts.map((product) => {
+        if (product.id === productId) {
+          return { ...product, quantity: newQuantity };
+        } else {
+          return product;
+        }
+      });
+
+      setFilteredProducts(updatedFilteredProducts);
+    }
+  };
+
   const handleFilterChange = (e) => {
     const selectedFilter = e.target.value;
-  
+
     if (selectedFilter === 'all') {
       setFilteredProducts(products);
     } else if (selectedFilter === 'Canned Goods') {
@@ -52,16 +74,16 @@ export default function Stocks() {
     } else if (selectedFilter === 'Hygiene Kit') {
       const filtered = products.filter((product) => product.category === 'Hygiene Kit');
       setFilteredProducts(filtered);
-    }
-     else if (selectedFilter === 'Soon-to-expire') {
-      const filtered = products.filter((product) => isWithinInterval(new Date(product.expiryDate), {
-        start: new Date(),
-        end: addWeeks(new Date(), 3)
-      }));
+    } else if (selectedFilter === 'Soon-to-expire') {
+      const filtered = products.filter((product) =>
+        isWithinInterval(new Date(product.expiryDate), {
+          start: new Date(),
+          end: addWeeks(new Date(), 3),
+        })
+      );
       setFilteredProducts(filtered);
     }
   };
-
   
 
   return (
@@ -93,7 +115,8 @@ export default function Stocks() {
             </div>
           </div>
           <div className='table-container'>
-          <ProductTable products={filteredProducts} />
+          <ProductTable products={filteredProducts} updateProductQuantity={updateProductQuantity} />
+
           </div>
         </div>
       </div>
--- e-stock/src/pages/stocks/table.js
+++ e-stock/src/pages/stocks/table.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.css';
 import { format } from 'date-fns';
 
-export default function Table({ products }) {
+export default function Table({ products, updateProductQuantity }) {
+  const [quantities, setQuantities] = useState({});
+
+  const handleQuantityChange = (productId, quantity) => {
+    setQuantities({ ...quantities, [productId]: quantity });
+    updateProductQuantity(productId, quantity);
+  };
+
   return (
     <table className='table'>
     <thead>
@@ -13,7 +20,6 @@ export default function Table({ products }) {
         <th>Date Added</th>
         <th>Expiry Date</th>
         <th>Quantity</th>
-       
       </tr>
     </thead>
     <tbody>
@@ -24,12 +30,17 @@ export default function Table({ products }) {
           <td>{product.category}</td>
           <td>{format(new Date(product.dateCreated), 'dd-MM-yyyy')}</td>
           <td>{format(new Date(product.expiryDate), 'dd-MM-yyyy')}</td>
-          <td style={{color:"#029801"}}>{product.quantity}</td>
-          
+          <td>
+            <input
+              type='number'
+              value={quantities[product.id] || product.quantity}
+              onChange={(e) => handleQuantityChange(product.id, e.target.value)}
+              style={{ color: "#029801", border:0, width:"50px", fontSize:"16px" }}
+            />
+          </td>
         </tr>
       ))}
     </tbody>
   </table>
-  
   );
 }

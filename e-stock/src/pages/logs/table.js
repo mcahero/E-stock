8fe56d4eedf3942e89fe44e7f2c2a6297306c9d7
Mark Dave <dave.lorejo69@gmail.com>
2023-04-21 23:13:44 +0800

    completed logs

--- e-stock/src/pages/logs/table.js
+++ e-stock/src/pages/logs/table.js
@@ -3,23 +3,45 @@ import { format } from 'date-fns';
 
 export default function Table() {
   const Products = JSON.parse(localStorage.getItem("products")) || [];
+  const Logs = JSON.parse(localStorage.getItem("logs")) || [];
 
   const calculateQuantityDiff = (product) => {
-    const diff = product.originalQuantity - product.quantity;
-  return isNaN(diff) || diff < 0 ? 0 : diff;
+    const logs = Logs.filter(log => log.productId === product.id);
+    const latestLog = logs[logs.length - 1];
+    const diff = latestLog ? latestLog.quantity - product.quantity : product.originalQuantity - product.quantity;
+    return isNaN(diff) || diff < 0 ? 0 : diff;
   }
+  
 
   const sortedProducts = Products.map((product) => {
+    const diff = calculateQuantityDiff(product);
+    const transactionId = isNaN(product.transactionId) ? product.id : product.transactionId;
+    const transactionDate = isNaN(product.transactionDate) ? product.dateCreated : product.transactionDate;
+
+    // create a new log entry for each update
+    if (diff > 0 && (!Logs.length || Logs[Logs.length - 1].quantity !== product.quantity)) {
+      const logEntry = {
+        transactionId: transactionId,
+        productId: product.id,
+        productName: product.productName,
+        quantity: product.quantity,
+        quantityRemoved: diff,
+        transactionDate: new Date().toISOString(),
+        modifiedBy: product.modifiedBy,
+      };
+      Logs.push(logEntry);
+      localStorage.setItem("logs", JSON.stringify(Logs));
+    }
+    
+
     return {
       ...product,
-      transactionId: isNaN(product.transactionId) ? product.id : product.transactionId,
-     
-      quantityRemoved: calculateQuantityDiff(product),
-      transactionDate: isNaN(product.transactionDate) ? product.dateCreated : product.transactionDate,
+      transactionId: transactionId,
+      quantityRemoved: diff,
+      transactionDate: transactionDate,
     };
   })
 
-  
   return (
     <table className='table'>
       <thead>
@@ -34,19 +56,32 @@ export default function Table() {
         </tr>
       </thead>
       <tbody>
-        {sortedProducts.map((product, index) => (
-          <tr key={index}>
-            <td>{product.transactionId}</td>
-            <td>{product.id}</td>
-            <td>{product.productName}</td>
-            <td>{product.transactionDate && format(new Date(product.transactionDate), 'dd MMMM yyyy')}</td>
-            
-            <td  style={{ color: "#029801" }}>{product.quantity}</td>
-            <td style={{ color: "#FF0100" }}>{product.quantityRemoved}</td>
-            <td>{product.modifiedBy}</td>
-         
-          </tr>
-        ))}
+        {sortedProducts.flatMap((product, index) => {
+          const productLogs = Logs.filter(log => log.productId === product.id);
+          const logs = [...productLogs, ...(product.logs || [])];
+          return [
+            <tr key={`${index}_product`}>
+              <td>{product.transactionId}</td>
+              <td>{product.id}</td>
+              <td>{product.productName}</td>
+              <td>{product.transactionDate && format(new Date(product.transactionDate), 'dd MMMM yyyy')}</td>
+              <td style={{ color: "#029801" }}>{product.originalQuantity}</td>
+              <td style={{ color: "#FF0100" }}>{product.quantityRemoved}</td>
+              <td>{product.modifiedBy}</td>
+            </tr>,
+            ...logs.map((log, i) => (
+              <tr key={`${index}_log_${i}`}>
+                <td>{log.transactionId}</td>
+                <td>{log.productId}</td>
+                <td>{log.productName}</td>
+                <td>{log.transactionDate && format(new Date(log.transactionDate), 'dd MMMM yyyy')}</td>
+                <td style={{ color: "#029801" }}>{log.quantity}</td>
+                <td style={{ color: "#FF0100" }}>{log.quantityRemoved}</td>
+                <td>{log.modifiedBy}</td>
+              </tr>
+            )),
+          ];
+        })}
       </tbody>
     </table>
   );
